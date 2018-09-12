6e4ec424a1478c30bf5c3df02e5aec71a71778da
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2018-09-13 03:14:50 +0600

    Modified gulpfile.js

--- README.md
+++ README.md
@@ -137,7 +137,7 @@ This is learning project about:
       or [gulp v3.9.1](https://github.com/gulpjs/gulp): Run `npm install --save-dev gulp` and Create `gulpfile.js` file.
       gulp v3.9.1 and gulp v4.0.0 are different in configuration syntax. In this project, we use [gulp v4.0.0 and its configurations](https://github.com/gulpjs/gulp/blob/master/docs/getting-started/3-creating-tasks.md).
    2. [gulp-jshint](https://www.npmjs.com/package/gulp-jshint): Run `npm install --save-dev jshint gulp-jshint`
-   3. [gulp-ruby-sass](https://www.npmjs.com/package/gulp-ruby-sass): Run `npm install --save-dev gulp-ruby-sass`
+   3. [gulp-dart-sass](https://www.npmjs.com/package/gulp-dart-sass): Run `npm install --save-dev gulp-dart-sass`
    4. [gulp-sourcemaps](https://www.npmjs.com/package/gulp-sourcemaps): Run `npm install --save-dev gulp-sourcemaps`
    5. [gulp-uglify](https://www.npmjs.com/package/gulp-uglify): Run `npm install --save-dev gulp-uglify`
    6. [gulp-connect](https://www.npmjs.com/package/gulp-connect): Run `npm install --save-dev gulp-connect`
@@ -145,6 +145,9 @@ This is learning project about:
    8. [jshint](https://www.npmjs.com/package/jshint): Run `npm install --save-dev jshint`
    9. [vinyl-buffer](https://www.npmjs.com/package/vinyl-buffer): Run `npm install --save-dev vinyl-buffer`
    10. [vinyl-source-stream](https://www.npmjs.com/package/vinyl-source-stream): Run `npm install --save-dev vinyl-source-stream`
+   11. [del](https://www.npmjs.com/package/del): Run `npm install --save-dev del`
+   12. [jshint-stylish](https://www.npmjs.com/package/jshint-stylish) Run `npm install --save-dev jshint-stylish`
+- Run gulp task command: `gulp`
 
 ## Setting up a folder structure:
    ```
@@ -154,9 +157,13 @@ This is learning project about:
             |- scripts.js
          |- scss
             |- styles.scss
-      |- css
-         |- styles.css
-      |- js
-         |- scripts.js
+      |- dist
+         |- css
+            |- styles.css
+         |- js
+            |- bundle.js
       |- index.html
    ```
+   
+## License
+ Single license MI
--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,156 @@
-var browserify = require('browserify'),
-    gulp = require('gulp'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
-    sass = require('gulp-ruby-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    webserver = require('gulp-connect');
+"use strict";
 
-gulp.task('js', function() {
-    var b = browserify({
-        entries: [
-            'node_modules/jquery/dist/jquery.js',
-            'assets/js/scripts.js'
-        ]
+const browserify = require('browserify');
+const gulp = require('gulp');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const gulpJshint = require('gulp-jshint');
+const dartSass = require('gulp-dart-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const webserver = require('gulp-connect');
+const del = require('del');
 
-    });
-    return b.bundle()
-            .pipe(source('bundle.js'))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(uglify())
-            .on('error', function(err) {
-                console.error('Error!', err.message);
-            })
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('./dist/js/'));
-    
-});
+/**
+ * Define the pathes
+ */
+const paths = {
+    styles: {
+        allScss: 'assets/scss/**/*.scss',
+        src: 'assets/scss/styles.scss',
+        dest: 'dist/css/'        
+    },
+    scripts: {
+        allJs: 'assets/js/**/*.js',
+        src: 'assets/js/scripts.js',
+        dest: 'dist/js/'     
+    },
+    htlm: {
+        root: 'index.html'
+    },
+    lib: {
+        jquery: 'node_modules/jquery/dist/jquery.js'
+    }
+};
 
-gulp.task('jshint', ['js'], function() {
-    return gulp.src('dist/js/bundle.js')
-        .pipe(jshint('./.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'));
-});
 
-gulp.task('sass', function() {
-    return sass('assets/sass/styles.scss', {
-        sourcemap: true,
-        style: 'expanded'
+/**
+ * Del
+ * 
+ * Clean the distribution files
+ * 
+ * https://www.npmjs.com/package/del
+ */
+function clean() {
+    return del([
+        'dist/js/bundle.js',
+        'dist/css/styles.css'
+    ]);
+}
+
+/**
+ * Browserify, Vinyl Source Stream, Vinyl Buffer, Gulp Uglify, Gulp Sourcemaps
+ * 
+ * Make a bundle.js file, minify this js file and place to distribution folder
+ *  
+ * https://www.npmjs.com/package/browserify
+ * https://www.npmjs.com/package/vinyl-source-stream
+ * https://www.npmjs.com/package/vinyl-buffer
+ * https://www.npmjs.com/package/gulp-uglify
+ * https://www.npmjs.com/package/gulp-sourcemaps
+ * 
+ */
+function bundleJs(cb) {
+    return browserify({
+        entries: [    
+            paths.scripts.src                    
+        ],
+        debug: true
     })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())    
     .on('error', function(err) {
-        console.error('Error!', err.message);
+        cb(new Error(err.message));
     })
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css/'));
-});
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(webserver.reload());
+}
 
-gulp.task('watch', function() {
-    gulp.watch('assets/js/**/*.js', ['js', 'jshint']);
-    gulp.watch('assets/sass/**/*.scss', ['scss']);
-});
+/**
+ * Gulp jshint
+ * 
+ * bundle.js code analysis
+ * 
+ * https://www.npmjs.com/package/jshint
+ * 
+ */
+function analysisJs() {
+    return gulp.src(paths.scripts.dest + 'bundle.js')
+        .pipe(gulpJshint('.jshintrc'))
+        .pipe(gulpJshint.reporter('jshint-stylish'));
+}
 
-gulp.task('webserver', function() {
-    webserver.server({
-        root: '/',
+/**
+ * Gulp Ruby Sass
+ * 
+ * Compile Sass to CSS using dart-sass
+ * 
+ * https://www.npmjs.com/package/gulp-ruby-sass
+ * https://github.com/sass/dart-sass
+ */
+function bundleCss(cb) {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(dartSass().on('error', function(err) {
+            cb(new Error(err.message));
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(webserver.reload()); 
+}
+
+function htmlLoad() {
+    return gulp.src(paths.htlm.root)
+        .pipe(webserver.reload());
+}
+
+/**
+ * Gulp Watch 
+ * Monitor files and excute tasks
+ * https://github.com/gulpjs/gulp/blob/master/docs/getting-started/8-watching-files.md
+ */
+function watch() {
+    // gulp.watch(paths.scripts.allJs, gulp.series(bundleJs, analysisJs));
+    gulp.watch(paths.scripts.allJs, bundleJs);
+    gulp.watch(paths.styles.allScss, bundleCss);
+    gulp.watch(paths.htlm.root, htmlLoad);
+}
+
+/**
+ * Gulp Connect
+ * Run a webserver 
+ * https://www.npmjs.com/package/gulp-connect
+ */
+function webServer() {
+    webserver.server({ 
+        root: '.',       
         livereload: true
     });
-});
+}
 
-gulp.task('default', ['watch', 'sass', 'webserver']);
+/**
+ * Create build series task
+ */
+// var build = gulp.series(clean, gulp.parallel(gulp.series(bundleJs, analysisJs), bundleCss));
+var build = gulp.series(clean, gulp.parallel(bundleJs, bundleCss));
+exports.build = build;
+exports.cls = clean;
+
+/**
+ * Create default task
+ */
+exports.default = gulp.series(build, gulp.parallel(webServer, watch));
